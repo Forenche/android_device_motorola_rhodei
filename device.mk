d27e5e85c461022d83da75b9b3aee4367334d185
Forenche <prahul2003@gmail.com>
2023-05-09 12:45:57 +0530

    Revert "[SQUASH]rhodei: Add refresh rate settings"
    This reverts commit 83c5d91d8adf70d9bb08fa8423a59eb1466e5c4d.
    
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- device.mk
+++ device.mk
@@ -30,14 +30,8 @@ PRODUCT_SYSTEM_PROPERTIES := \
 PRODUCT_PROPERTY_OVERRIDES := \
     ro.sf.lcd_density=420
 
-# SurfaceFlinger offesets
+# Vendor props
 PRODUCT_VENDOR_PROPERTIES += \
-	ro.surface_flinger.set_idle_timer_ms=250 \
-	ro.surface_flinger.set_touch_timer_ms=200 \
-	ro.surface_flinger.use_color_management=true \
-	ro.surface_flinger.use_content_detection_for_refresh_rate=true \
-	ro.surface_flinger.use_smart_90_for_video=true \
-	ro.surface_flinger.wcg_composition_dataspace=143261696 \
 	zygote.critical_window.minute=10 \
 	DEVICE_PROVISIONED=1
 
